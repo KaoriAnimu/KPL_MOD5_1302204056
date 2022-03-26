48376b43de8ab82372ffa1ecee5996d261149d83
KaoriAnimu <mkaorirahadj89@gmail.com>
2022-03-26 17:34:21 +0700

    Penambahan desain by contract pada semua kelas

---
+++ KPLMOD5 Design/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+
+
+namespace KPLMOD5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            WatashiNoBideo s = new WatashiNoBideo ("\n Film Review of Fate Grand Order by Moch. Kaori Rahadian ") ;
+
+            Console.WriteLine();
+
+            WatashiNoBideo s1 = new WatashiNoBideo ("\n Film Review of Fate Stay Night by Moch. Kaori Rahadian ") ;
+
+            Console.WriteLine();
+
+            WatashiNoBideo s2 = new WatashiNoBideo("\n Film Review of Fate Zero by Moch. Kaori Rahadian ");
+
+            Console.WriteLine();
+
+            WatashiNoBideo s3 = new WatashiNoBideo("\n Film Review of Fate Apocrypha by Moch. Kaori Rahadian ");
+
+            Console.WriteLine();
+
+            WatashiNoBideo s4 = new WatashiNoBideo("\n Film Review of Fate Extra by Moch. Kaori Rahadian ");
+
+            Console.WriteLine();
+
+            WatashiNoBideo s5 = new WatashiNoBideo("\n Film Review of Taimanin Asagi Night by Moch. Kaori Rahadian ");
+
+            Console.WriteLine();
+
+            WatashiNoBideo s6 = new WatashiNoBideo("\n Film Review of Hell Knight Inggrid by Moch. Kaori Rahadian ");
+
+            Console.WriteLine();
+
+            WatashiNoBideo s7 = new WatashiNoBideo("\n Film Review of Pisu Hame by Moch. Kaori Rahadian ");
+
+            Console.WriteLine();
+
+            WatashiNoBideo s8 = new WatashiNoBideo("\n Film Review of OniChiChi by Moch. Kaori Rahadian ");
+
+            Console.WriteLine();
+
+            WatashiNoBideo s9 = new WatashiNoBideo("\n Film Review of Kuroinu by Moch. Kaori Rahadian ");
+
+            Console.WriteLine();
+
+            WatashiNoBideo s10 = new WatashiNoBideo("\n Film Review of Tokubetsu Jugyou 3 by Moch. Kaori Rahadian ");
+
+            Console.WriteLine();
+
+            WatashiNoYusaa ss = new WatashiNoYusaa (" Moch. Kaori Rahadian ") ;
+
+            s.IncreaseSaiseikaisu(1) ;
+
+            s1.IncreaseSaiseikaisu(10) ;
+
+            ss.AddVideo(s) ;
+
+            ss.AddVideo(s1) ;
+
+            ss.AddVideo(s2) ;
+
+            ss.AddVideo(s3) ;
+
+            ss.AddVideo(s4) ;
+
+            ss.AddVideo(s5) ;
+
+            ss.AddVideo(s6) ;
+
+            ss.AddVideo(s7) ; 
+
+            ss.AddVideo(s8) ;
+
+            ss.AddVideo(s9) ;
+
+            ss.AddVideo(s10) ;
+
+            s.PrintVideoDetails() ;
+
+            ss.PrintAllVideoSaiseikaisu() ;
+
+        }
+    }
+}
---
+++ KPLMOD5 Design/WatashiNoBideo.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KPLMOD5
+{
+    internal class WatashiNoBideo
+    {
+        private int id ;
+
+        private String taitoru ;
+
+        private int Saiseikaisu ;
+
+        public WatashiNoBideo(String taitoru)
+        {
+
+            Random random = new Random() ;
+
+            this.id = random.Next(10000, 99999) ;
+
+            if (taitoru == "")
+
+                throw new NullReferenceException(" Null is not allowed for tittle ") ;
+
+            if (taitoru.Length > 200)
+
+                throw new Exception(" The maximum length of the text is 100 Characters ") ;
+
+            this.taitoru = taitoru ;
+
+
+            this.Saiseikaisu = 0 ;
+
+        }
+
+        public void IncreaseSaiseikaisu(int x)
+        {
+            if (x > 25000000 && x == -1)
+
+                throw new NullReferenceException(" Null is not allowed for tittle ") ;
+
+            this.Saiseikaisu = x ;
+
+        }
+
+        public int getSaiseikaisu()
+        {
+
+            return this.Saiseikaisu ;
+
+        }
+
+        public String gettaitoru()
+        {
+
+            return this.taitoru ;
+
+        }
+
+        public void PrintVideoDetails()
+        {
+            for (int x = 0; x < Saiseikaisu; x++)
+            {
+
+                Console.WriteLine(" Bideo ID : " + this.id) ;
+
+                Console.WriteLine();
+
+                Console.WriteLine(" Bideo taitoru : " + this.taitoru) ;
+
+                Console.WriteLine();
+
+                Console.WriteLine(" Bideo Saiseikaisu : " + (x+1)) ;
+
+                Console.WriteLine() ;
+
+            }
+
+
+        }
+
+    }
+}
